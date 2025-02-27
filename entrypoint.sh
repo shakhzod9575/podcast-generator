a663d509e9db7c5a4954e91c3056778f1f600af9
shakhzod9575 <153494813+shakhzod9575@users.noreply.github.com>
2025-02-27 21:23:15 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,11 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
+# Activate the virtual environment
+source /venv/bin/activate
+
 # Run the Python script
-python3 /usr/bin/feed.py
+python /usr/bin/feed.py
 
 # Commit and push changes
 git add -A
