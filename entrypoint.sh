873229dc321aad9793e7c11830e988dd5023aa8a
shakhzod9575 <153494813+shakhzod9575@users.noreply.github.com>
2025-02-27 21:19:15 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+set -e  # Exit on error
 
 echo "=================="
 
+# Configure Git
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
+# Run the Python script
 python3 /usr/bin/feed.py
 
-git add -A && commit -m "Update Feed"
-
+# Commit and push changes
+git add -A
+git commit -m "Update Feed"
 git push --set-upstream origin main
 
 echo "=================="
